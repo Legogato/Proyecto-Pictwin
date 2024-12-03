5388560b097535c611b14147726e8c1fedf4311b
Legogato <perez.pabloignacio@gmail.com>
2024-12-03 00:58:41 -0300

    fix(network module): fixed network module imports

--- PicTwin/app/src/main/java/cl/ucn/disc/dsm/pictwin/data/network/NetworkModule.kt
+++ PicTwin/app/src/main/java/cl/ucn/disc/dsm/pictwin/data/network/NetworkModule.kt
@@ -4,6 +4,21 @@
 
 package cl.ucn.disc.dsm.pictwin.data.network
 
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.slf4j.LoggerFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {

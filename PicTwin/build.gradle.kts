65e68cb0e26e222448d53c198c50f71ebec72b54
Legogato <perez.pabloignacio@gmail.com>
2024-12-02 23:49:06 -0300

    update(build.gradle.kts): updated build.grade.kts(pictwin))

--- PicTwin/build.gradle.kts
+++ PicTwin/build.gradle.kts
@@ -1,6 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+/*
+ * Copyright (c) 2024. Desarrollo de Soluciones Moviles, DISC.
+ */
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+
+    alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.ksp) apply false
 }
